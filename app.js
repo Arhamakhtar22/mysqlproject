015a183abcb6e3bdf173e8e3cfa126d48b49eaf9
Arham Shoaib <arhamshoaib22@outlook.com>
2022-10-27 15:40:18 -0700

    post route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
  var connection = mysql.createConnection({
     host: "localhost",
@@ -10,30 +14,23 @@ var app = express();
     database: 'join_us'
 });
 
-
-/* var data = [];
-for(var i=0; i < 500; i++){
-    data.push([
-        faker.internet.email(),
-        faker.date.past()
-    ]);
-} */
-
-
-
-//connection.query(q, [data], function(error, result) {
-    //if (error) throw error;
-    //console.log(result);
-//});
-
-/* connection.end(); */
-
 app.get("/", function(req, res){
     var q = 'SELECT COUNT(*) AS count FROM users'
     connection.query(q, function (err, results){
         if (err) throw err;
         var count = results[0].count;
-        res.send("We have " + count + " users in our db")
+        //res.send("We have " + count + " users in our db")
+        res.render("home",{data: count});
+    });
+});
+
+app.post("/register", function(req, res){
+    var new_user = {
+        email:req.body.email
+    };
+    connection.query('INSERT INTO users SET ?', new_user, function(err, result){
+        if (err) throw err;
+        res.redirect("/");
     });
 });
 
